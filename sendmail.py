3206c0d868ad8e0ea71bf61c1633894efc4bd60f
Cactooz <54435874+Cactooz@users.noreply.github.com>
2020-10-29 11:28:20 +0100

    Extra mail messsage information

--- sendmail.py
+++ sendmail.py
@@ -1,8 +1,17 @@
 #Import libraries
 import smtplib
 import sys
+import getpass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from datetime import datetime
+
+#-------DESCRIPTION--------
+#Sends a mail with a logfile to the specified email.
+#Needs to be set up with emails, server, username and password.
+
+#-----COMMAND LAYOUT-------
+#python3 ./sendmail.py FILEPATH FILENAME MAILSUBJECT
 
 #---------CONFIG-----------
 #Email address that sends the email
@@ -36,17 +45,37 @@ serverip = "{}:{}".format(ip, port)
 #Combind the filepath and filename
 fullpath = "{}/{}".format(filepath, filename)
 
-#Read logfile
+#Array with message lines
+message = []
+
+#Add base message
+message.append("Auto mail about: {}\n".format(mailsubject))
+message.append("Log: {}/{}\n\n".format(filepath, filename))
+message.append("Log information can be seen bellow.\n\n")
+message.append("========================\n")
+message.append("	START OF LOG FILE\n")
+message.append("========================\n\n")
+
+#Read log file and add to message
 file = open(fullpath,'r')
-log = []
 lines = file.readlines()
 for line in lines:
-    log.append(line)
+    message.append(line)
     file.close()
 
-log = (str("".join(log)))
-#print(log)
+#Add file end message
+message.append("\n========================\n")
+message.append("	END OF LOG FILE\n")
+message.append("========================\n\n")
+message.append("For more information see full and/or other logfiles.\n")
+message.append("Logfiles location: {}\n".format(filepath))
+message.append("Automatic mail sent by {} at {}.".format(getpass.getuser(), datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
 
+#Make message into a string
+message = (str("".join(message)))
+#print(message)
+
+#Setup the mail message
 msg = MIMEMultipart("alternative")
 msg.set_charset("utf-8")
 
@@ -55,8 +84,8 @@ msg["Subject"] = "{} - {}".format(mailsubject, filename)
 msg["From"] = fromaddress
 msg["To"] = toaddress
 
-#Add the logfile to the mail body (text)
-body = MIMEText(log, "plain", "utf-8")
+#Add the log file to the mail body (text)
+body = MIMEText(message, "plain", "utf-8")
 msg.attach(body)
 
 #Send the mail
