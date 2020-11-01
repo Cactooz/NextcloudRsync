05ecadcfd2590e78c82edecf8d8cf50a1ca90c16
Cactooz <54435874+Cactooz@users.noreply.github.com>
2020-11-01 18:42:07 +0100

    Errorcheck & small changes

--- weeklymail.sh
+++ weeklymail.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-DATE=$(date +%Y-%m-%d-%H-%M)
+DATE=$(date +%a-%d-%b-%H-%M)
 WEEK=$(date +%V)
+
+#-------DESCRIPTION--------
+#Sends a weekly mail with the weeks log.
 
 #---------CONFIG-----------
 #Name of the weekly logfile (default: "week-$WEEK-Rsync.log")
-WEEKLOGFILE="week-$WEEK-Rsync.log"
+WEEKLOGFILE="w$WEEK-Rsync.log"
 
 #Path to logfile (default: /var/log/rsync)
 LOGPATH=/var/log/rsync
 
 #Name of the logfile
-LOGFILE="$WEEK-mail-Rsync.log"
+LOGFILE="w$WEEK-mail-Rsync.log"
 
 #--------------------------
 
 echo "--- $DATE ---" >> $LOGPATH/$LOGFILE
 
-echo "Sending mail with this weeks Rsync log" | tee -a $LOGPATH/$LOGFILE
-python3 ./sendmail.py "$LOGPATH" "$WEEKLOGFILE" "Weekly Rsync" | tee -a $LOGPATH/$LOGFILE
-echo "Sent mail with this weeks Rsync log" | tee -a $LOGPATH/$LOGFILE
+echo "$(date +%T) - Sending mail with this weeks Rsync log" | tee -a $LOGPATH/$LOGFILE
+python3 ./sendmail.py "$LOGPATH" "$WEEKLOGFILE" "Weekly Rsync" 2>&1 | tee -a $LOGPATH/$LOGFILE
+if [ "$?" -eq "0" ]
+	then
+		echo "*** Mail Success *** Returncode: $?" >> $LOGPATH/$LOGFILE
+		echo "$(date +%T) - Sent mail with this weeks Rsync log" | tee -a $LOGPATH/$LOGFILE
+		echo "$(date +%T) - Sent mail with this weeks Rsync log" >> $LOGPATH/$WEEKLOGFILE
+	else
+		echo "*** Mail Fail *** Returncode: $?" >> $LOGPATH/$LOGFILE
+		echo "$(date +%T) - Failed sending mail with this weeks Rsync log" | tee -a $LOGPATH/$LOGFILE
+		echo "$(date +%T) - Failed sending mail with this weeks Rsync log" >> $LOGPATH/$WEEKLOGFILE
+fi
+
+echo "" >> $LOGPATH/$LOGFILE
+echo "" >> $LOGPATH/$WEEKLOGFILE
