2c9f3dc1d0aebcaf486c383ca3ab6c3cf4f70073
Cactooz <54435874+Cactooz@users.noreply.github.com>
2020-12-13 22:04:12 +0100

    Added send Discord webhook embed

---
+++ webhook.py
@@ -0,0 +1,69 @@
+#Import libraries
+import sys
+import datetime
+from discordwebhook import Discord
+
+#-------DESCRIPTION--------
+#Sends a embed to a Discord webhook with the logfile attached.
+#Needs to be set up with a Discord webhook url.
+
+#-----COMMAND LAYOUT-------
+#python3 ./webhook.py FILEPATH FILENAME TITLE COLOR
+
+#---------CONFIG-----------
+#Webhook url
+webhookurl = "https://discordapp.com/api/webhooks/<discord-webhook-url>"
+
+#--------------------------
+
+#Sending help command
+if sys.argv[1] == "-h":
+	print("DESCRIPTION\nSends a embed to a Discord webhook with the logfile attached.\nNeeds to be set up with a Discord webhook url.")
+	print("\nCOMMAND LAYOUT\npython3 ./webhook.py FILEPATH FILENAME TITLE COLOR")
+	exit(0)
+
+#Path to logfile
+filepath = sys.argv[1]
+
+#Name of the logfile
+filename = sys.argv[2]
+
+#Title for the embed
+title = sys.argv[3]
+
+#Description of the embed with log file
+description = "Log file: {}".format(filename)
+
+#Color of the webhook, red = 16711680 green = 65280
+color = sys.argv[4]
+
+log = "blah blah blah"
+
+#Footer text for the embed
+footer = "Log location: {}".format(filepath)
+
+#Attachment to send along the embed
+attachment = "{}/{}".format(filepath, filename)
+
+#Setup Discord webhook link
+discord = Discord(url=webhookurl)
+
+#Send Discord embed post
+discord.post(
+	embeds=[
+		{
+			"title": title,
+			"description": description,
+			"timestamp": str(datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()),
+			"color": color,
+			"fields": [
+				{"name": "Log File", "value": log, "inline": False},
+			],
+			"footer": {
+				"text": footer,
+			},
+		}
+	],
+	file={"file": open(attachment, "rb"),
+	},
+)
